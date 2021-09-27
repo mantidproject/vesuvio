6c5e84a7a37ae9ba1ca127d088c229043792fed2
Gui <gjmacielpereira1@sheffield.ac.uk>
2021-09-27 19:01:04 +0100

    saved some small change

--- backward/current_testing.py
+++ backward/current_testing.py
@@ -43,18 +43,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use('dark_background')
 
-syn = np.load(r"C:/Users/guijo/Desktop/optimizations/scaling_parameters.npz")
+syn = np.load(r"C:/Users/guijo/Desktop/optimizations/scaling_parameters_improved.npz")
 #np.load(r"C:\Users\guijo\Desktop\work_repos\scatt_scripts\backward\script_runs\opt_spec3-134_iter4_ncp_nightlybuild_synthetic.npz")
 
 ws = syn["all_fit_workspaces"][0, :, :-1]
 ncp = syn["all_tot_ncp"][0]
 
+ws = np.where(ws == 0, np.nan, ws)
 meanChi2 = np.nanmean(ws[:, -2])
 print("Mean Chi2 for 134 spectrums is: ", meanChi2)
 
 x = np.linspace(0, 1, len(ncp[0]))
 plt.figure(3)
-spec_idx = 0
+spec_idx = 15
+print("first values ws: ", ws[spec_idx, :5])
+print("first values ncp: ", ncp[spec_idx, :5])
+
 plt.plot(x, ws[spec_idx], label="synthetic ncp", linewidth = 2)
 plt.plot(x, ncp[spec_idx], "--", label="fitted ncp", linewidth = 2)
 plt.legend()
