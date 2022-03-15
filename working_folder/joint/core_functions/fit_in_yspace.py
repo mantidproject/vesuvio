Tried some more on the numba side, realized that manual minos is not working, and is producing wrong results compared to very recent commits.

working_folder/joint/core_functions/fit_in_yspace.py
@@ -190,7 +190,7 @@ def fitProfileMinuit(ic, wsYSpaceSym, wsRes):
                     -480*((x-x0)/np.sqrt(2)/sigma1)**4 + 720*((x-x0)/np.sqrt(2)/sigma1)**2 - 120))
         
         funcSig = ["x", "A", "x0", "sigma1", "c4", "c6"]
-        initPars = {"A":1, "x0":0, "sigma1":5, "c4":0, "c6":0}
+        initPars = {"A":1, "x0":0, "sigma1":4, "c4":0, "c6":0}
 
     xDense, xDelta, resDense = chooseXDense(resX, resY, True)
 
@@ -404,8 +404,12 @@ def runMinosForPar(minuitObj, constrFunc, var:str, bound:int, ax, bestFitVals, b
     # Store fval of best fit
     fValsMin = minuitObj.fval      # Used to calculate error bands at the end
 
-    # Initiate arrays
-    varSpace = np.linspace(varVal - bound*varErr, varVal + bound*varErr, 30)
+    # Create variable space more dense near the minima
+    # limit = (bound*varErr)**(1/2)
+    # varSpace = np.linspace(-limit, limit, 30)
+    # varSpace = varSpace**2 * np.sign(varSpace) + varVal
+    varSpace = np.linspace(varVal-bound*varErr, varVal+bound*varErr, 30)
+
     fValsScipy = np.zeros(varSpace.shape)
     fValsMigrad = np.zeros(varSpace.shape)
 

working_folder/joint/scribles/group_dets.py
@@ -14,6 +14,7 @@ idx = np.argsort(ipData[:, -1])
 
 dets = ipData[idx, 0]
 L1sorted = ipData[idx, -1]
+thetaDets = ipData[idx, 2]
 print(L1sorted[:5])
 
 reltol = np.abs((L1sorted[1:] - L1sorted[:-1])/L1sorted[:-1])
@@ -28,6 +29,7 @@ print("len of first half: ", len(firstHalf), "last item: ", firstHalf[-1])
 print("len of first half: ", len(secondHalf), "first item: ", secondHalf[-1])
 
 # plt.plot(L1sorted[:-1], reltol, "ko")
-plt.plot(L1sorted, np.ones(L1sorted.size), "ko")
+plt.plot(L1sorted, thetaDets, "ko")
 plt.xlabel("L1")
+plt.ylabel("Theta")
 plt.show()

working_folder/joint/scribles/scibles.py
@@ -1,12 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-fig, (ax1, ax2)  = plt.subplots(1, 2)
-ax1.plot([1, 3, 5], color='blue', label="blue")
-ax1.plot([2, 4, 6], color='red', label="red")
+# fig, (ax1, ax2)  = plt.subplots(1, 2)
+# ax1.plot([1, 3, 5], color='blue', label="blue")
+# ax1.plot([2, 4, 6], color='red', label="red")
 
-handle, label = ax1.get_legend_handles_labels()
-print(handle, label)
-fig.legend(handle, label)
+# handle, label = ax1.get_legend_handles_labels()
+# print(handle, label)
+# fig.legend(handle, label)
 
-plt.show()
+# plt.show()
+
+r = 5
+zero = 3
+n = 30
+x = np.linspace(-r**(1/3), r**(1/3), n)
+y = x**3
+
+print(x[:5])
+print(y[:5])