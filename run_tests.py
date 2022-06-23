e087d4122307cfb64a42e221b6fecf15d78df6cb
Gui <gui.maciel-pereira@stfc.ac.uk>
2022-06-23 16:58:04 +0100

    Updated the tests.

--- run_tests.py
+++ run_tests.py
@@ -11,10 +11,10 @@ import vesuvio_analysis.tests.test_analysis as analysis
 # Add tests to the test suite
 suite.addTests(loader.loadTestsFromModule(analysis))
 
-# import vesuvio_analysis.tests.test_yspace_fit as yspacefit
-# suite.addTests(loader.loadTestsFromModule(yspacefit))
-# import vesuvio_analysis.tests.test_yspace_fit_GC as yspacefit_GC
-# suite.addTests(loader.loadTestsFromModule(yspacefit_GC))
+import vesuvio_analysis.tests.test_yspace_fit as yspacefit
+suite.addTests(loader.loadTestsFromModule(yspacefit))
+import vesuvio_analysis.tests.test_yspace_fit_GC as yspacefit_GC
+suite.addTests(loader.loadTestsFromModule(yspacefit_GC))
 
 import vesuvio_analysis.tests.test_bootstrap as bootstrap
 suite.addTests(loader.loadTestsFromModule(bootstrap))
