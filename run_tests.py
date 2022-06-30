b2f94aab14137835cfb13bf95b00eaf4d3724e8a
Gui Maciel Pereira <gjmacielpereira1@sheffield.ac.uk>
2022-06-30 11:56:08 +0100

    Uncommented some tests.

--- run_tests.py
+++ run_tests.py
@@ -13,14 +13,14 @@ suite.addTests(loader.loadTestsFromModule(analysis))
 
 import vesuvio_analysis.tests.test_yspace_fit as yspacefit
 suite.addTests(loader.loadTestsFromModule(yspacefit))
-# import vesuvio_analysis.tests.test_yspace_fit_GC as yspacefit_GC
-# suite.addTests(loader.loadTestsFromModule(yspacefit_GC))
+import vesuvio_analysis.tests.test_yspace_fit_GC as yspacefit_GC
+suite.addTests(loader.loadTestsFromModule(yspacefit_GC))
 
-# import vesuvio_analysis.tests.test_bootstrap as bootstrap
-# suite.addTests(loader.loadTestsFromModule(bootstrap))
+import vesuvio_analysis.tests.test_bootstrap as bootstrap
+suite.addTests(loader.loadTestsFromModule(bootstrap))
 
-# import vesuvio_analysis.tests.test_jackknife as jackknife
-# suite.addTests(loader.loadTestsFromModule(jackknife))
+import vesuvio_analysis.tests.test_jackknife as jackknife
+suite.addTests(loader.loadTestsFromModule(jackknife))
 
 
 # Initialize a runner, pass it your suite and run it
