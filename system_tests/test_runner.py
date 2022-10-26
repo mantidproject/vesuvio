2662924d33c7429cf963751ff9d4e8da99e4aec0
MialLewis <95620982+MialLewis@users.noreply.github.com>
2022-10-26 17:42:22 +0100

    update test runner and workflow to fail github action

--- system_tests/test_runner.py
+++ system_tests/test_runner.py
@@ -10,18 +10,21 @@ import system_tests.test_yspace_fit as yspacefit
 import system_tests.test_yspace_fit_GC as yspacefitGC
 import unittest
 
-# Initialize the test suite
-loader = unittest.TestLoader()
-suite = unittest.TestSuite()
 
-# Import modules to be tested
-# Add tests to the test suite
-suite.addTests(loader.loadTestsFromModule(analysis))
-suite.addTests(loader.loadTestsFromModule(bootstrap))
-suite.addTests(loader.loadTestsFromModule(jackknife))
-suite.addTests(loader.loadTestsFromModule(yspacefit))
-suite.addTests(loader.loadTestsFromModule(yspacefitGC))
+def run():
+    # Initialize the test suite
+    loader = unittest.TestLoader()
+    suite = unittest.TestSuite()
 
-# Initialize a runner, pass it your suite and run it
-runner = unittest.TextTestRunner(verbosity=1)
-result = runner.run(suite)
+    # Import modules to be tested
+    # Add tests to the test suite
+    suite.addTests(loader.loadTestsFromModule(analysis))
+    suite.addTests(loader.loadTestsFromModule(bootstrap))
+    suite.addTests(loader.loadTestsFromModule(jackknife))
+    suite.addTests(loader.loadTestsFromModule(yspacefit))
+    suite.addTests(loader.loadTestsFromModule(yspacefitGC))
+
+    # Initialize a runner, pass it your suite and run it
+    runner = unittest.TextTestRunner(verbosity=1)
+    result = runner.run(suite)
+    return result
