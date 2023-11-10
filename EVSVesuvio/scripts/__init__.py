3de46ad225dddd67f27aef580ad19fff5c5ca524
MialLewis <95620982+MialLewis@users.noreply.github.com>
2023-11-10 15:18:18 +0000

    add config dir creation

--- EVSVesuvio/scripts/__init__.py
+++ EVSVesuvio/scripts/__init__.py
@@ -1,9 +1,20 @@
 """Package defining top-level application
 and entry points.
 """
+import os
+from shutil import copyfile
 
 
 def main():
-    """Placeholder.
-    """
-    print("TEST_VESUVIO_LAUNCH")
+    make_config_dir()
+
+
+def make_config_dir():
+    config_dir = os.path.join(os.path.expanduser("~"), '.mvesuvio')
+    if not os.path.isdir(config_dir):
+        os.mkdir(config_dir)
+        copyfile('EVSVesuvio/config/vesuvio.user.properties', f'{config_dir}/vesuvio.user.properties')
+
+
+if __name__ == '__main__':
+    main()
