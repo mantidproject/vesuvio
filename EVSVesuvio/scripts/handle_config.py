3a712262921a7c27716a3c19f9cfaae3a4b88168
GuiMacielPereira <gui.maciel-pereira@stfc.ac.uk>
2023-12-13 16:11:11 +0000

    Revert "Cleaned scripts for CLI"
    This reverts commit 42a67076bfc3d1193fdddf42228a568babc5fee5.

--- EVSVesuvio/scripts/handle_config.py
+++ EVSVesuvio/scripts/handle_config.py
@@ -9,40 +9,18 @@ MANTID_CONFIG_FILE = "Mantid.user.properties"
 VESUVIO_IPFOLDER_PATH = os.path.join(VESUVIO_CONFIG_PATH, "ip_files")
 
 
-def setup_default_config():
-    success = __mk_dir('config', VESUVIO_CONFIG_PATH)
-    if success:
-        copytree(os.path.join(VESUVIO_PACKAGE_PATH, "config", "ip_files"), VESUVIO_IPFOLDER_PATH, ignore=ignore_patterns('__*'))
-        set_expr_dir(VESUVIO_CONFIG_PATH, "default")
-
-        copyfile(os.path.join(VESUVIO_PACKAGE_PATH, "config", VESUVIO_CONFIG_FILE), os.path.join(VESUVIO_CONFIG_PATH, VESUVIO_CONFIG_FILE))
-        copyfile(os.path.join(VESUVIO_PACKAGE_PATH, "config", MANTID_CONFIG_FILE), os.path.join(VESUVIO_CONFIG_PATH, MANTID_CONFIG_FILE))
-
-        set_config_vars({'caching.location': VESUVIO_CONFIG_PATH,
-                         'caching.experiment': "default",
-                         'caching.ipfolder': VESUVIO_IPFOLDER_PATH}, verbose=False)
+def __read_config(config_file_path, throw_on_not_found=True):
+    lines = ""
+    try:
+        with open(config_file_path, 'r') as file:
+            lines = file.readlines()
+    except IOError:
+        if throw_on_not_found:
+            raise RuntimeError(f"Could not read from vesuvio config file: {config_file_path}")
+    return lines
 
 
-def set_expr_dir(cache_dir, experiment):
-    expr_path = os.path.join(cache_dir, "experiments", experiment)
-    success = __mk_dir('experiment', expr_path)
-    if success:
-        copyfile(os.path.join(VESUVIO_PACKAGE_PATH, "config", VESUVIO_INPUTS_FILE),
-                 os.path.join(cache_dir, "experiments", experiment, VESUVIO_INPUTS_FILE))
-
-
-def __mk_dir(type, path):
-    success = False
-    if not os.path.isdir(path):
-        try:
-            os.makedirs(path)
-            success = True
-        except:
-            print(f'Unable to make {type} directory at location: {path}')
-    return success
-
-
-def set_config_vars(var_dict, verbose=True):
+def set_config_vars(var_dict):
     file_path = os.path.join(VESUVIO_CONFIG_PATH, VESUVIO_CONFIG_FILE)
     lines = __read_config(file_path)
 
@@ -54,8 +32,7 @@ def set_config_vars(var_dict, verbose=True):
                 new_line = f'{var}={var_dict[var]}'
                 updated_lines.append(f'{new_line}\n')
                 match = True
-                if verbose:
-                    print(f'Setting: {new_line}')
+                print(f'Setting: {new_line}')
                 break
 
         if not match:
@@ -79,26 +56,43 @@ def read_config_var(var, throw_on_not_found=True):
     return result
 
 
-def __read_config(config_file_path, throw_on_not_found=True):
-    lines = ""
-    try:
-        with open(config_file_path, 'r') as file:
-            lines = file.readlines()
-    except IOError:
-        if throw_on_not_found:
-            raise RuntimeError(f"Could not read from vesuvio config file: {config_file_path}")
-    return lines
+def setup_config_dir(config_dir):
+    success = __mk_dir('config', config_dir)
+    if success:
+        copyfile(os.path.join(VESUVIO_PACKAGE_PATH, "config", VESUVIO_CONFIG_FILE), os.path.join(config_dir, VESUVIO_CONFIG_FILE))
+        copyfile(os.path.join(VESUVIO_PACKAGE_PATH, "config", MANTID_CONFIG_FILE), os.path.join(config_dir, MANTID_CONFIG_FILE))
+
+
+def setup_expr_dir(cache_dir, experiment):
+    expr_path = os.path.join(cache_dir, "experiments", experiment)
+    if os.path.isdir(expr_path):
+        return   # Avoids overwriting
+    __mk_dir('experiment', expr_path)
+    copyfile(os.path.join(VESUVIO_PACKAGE_PATH, "config", VESUVIO_INPUTS_FILE), input_file_path(cache_dir, experiment))
+
+
+def setup_default_ipfile_dir():
+    if not os.path.isdir(VESUVIO_IPFOLDER_PATH):
+        copytree(os.path.join(VESUVIO_PACKAGE_PATH, "config", "ip_files"), VESUVIO_IPFOLDER_PATH, ignore=ignore_patterns('__*'))
+
+
+def __mk_dir(type, path):
+    success = False
+    if not os.path.isdir(path):
+        try:
+            os.makedirs(path)
+            success = True
+        except:
+            print(f'Unable to make {type} directory at location: {path}')
+    return success
 
 
 def config_set():
-    if read_config_var('caching.location', False):
+    if(read_config_var('caching.location', False)):
         return True
     else:
         return False
 
 
-def check_dir_exists(type, path):
-    if not os.path.isdir(path):
-        print(f"Directory of {type} could not be found at location: {path}")
-        return False
-    return True
+def input_file_path(cache_dir, experiment):
+    return os.path.join(cache_dir, "experiments", experiment, VESUVIO_INPUTS_FILE)
