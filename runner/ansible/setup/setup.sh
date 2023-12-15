ffc24e8ad9031dcb9b27d402fa1d3316808a0545
MialLewis <95620982+MialLewis@users.noreply.github.com>
2023-12-15 18:49:12 +0000

    run script in users home

--- runner/ansible/setup/setup.sh
+++ runner/ansible/setup/setup.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 set -ex
-mkdir actions-runner && cd actions-runner
+mkdir /home/nonroot/actions-runner && cd /home/nonroot/actions-runner
 curl -o actions-runner-linux-x64-2.311.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.311.0/actions-runner-linux-x64-2.311.0.tar.gz
 tar xzf ./actions-runner-linux-x64-2.311.0.tar.gz
 ./config.sh --url https://github.com/mantidproject/vesuvio --token AWZQ65UVPZUA3LFSZDMHE4DFPSK5O
