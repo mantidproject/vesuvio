120581170969d1c15fa359b5cf5fd79a2efaf522
MialLewis <95620982+MialLewis@users.noreply.github.com>
2023-12-16 00:25:00 +0000

    use new runner token

--- runner/ansible/setup/setup.sh
+++ runner/ansible/setup/setup.sh
@@ -3,5 +3,5 @@ set -ex
 mkdir /home/nonroot/actions-runner && cd /home/nonroot/actions-runner
 curl -o actions-runner-linux-x64-2.311.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.311.0/actions-runner-linux-x64-2.311.0.tar.gz
 tar xzf ./actions-runner-linux-x64-2.311.0.tar.gz
-./config.sh --url https://github.com/mantidproject/vesuvio --token AWZQ65XDZQJX3FCADGXB2WDFPTZF6
+./config.sh --url https://github.com/mantidproject/vesuvio --token AWZQ65UNHU44YBHS7GRCB7LFPT6D4
 ./run.sh
