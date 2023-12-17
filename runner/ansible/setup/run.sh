97a8b647c3d5895a043890033cc2adcbec26787d
MialLewis <95620982+MialLewis@users.noreply.github.com>
2023-12-17 23:18:08 +0000

    Revert "seperate run and setup scripts"
    This reverts commit 4bd15a6466ef759cd88191a5f248c671c7c1b5e4.

--- runner/ansible/setup/run.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-set -ex
-mkdir /home/nonroot/actions-runner && cd /home/nonroot/actions-runner
-curl -o actions-runner-linux-x64-2.311.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.311.0/actions-runner-linux-x64-2.311.0.tar.gz
-tar xzf ./actions-runner-linux-x64-2.311.0.tar.gz
-./run.sh
