b328c8b3c28d814ea12b62c2c4cec467d1bcaf45
GuiMacielPereira <gui.maciel-pereira@stfc.ac.uk>
2024-01-03 12:52:06 +0000

    Made script run return results (used for testing)

--- mvesuvio/analysis_runner.py
+++ mvesuvio/analysis_runner.py
@@ -27,7 +27,7 @@ def run(yes_to_all=False):
     bootIC = ai.BootstrapInitialConditions
     userCtr = ai.UserScriptControls
 
-    runScript(
+    scattResults, yFitResults = runScript(
         userCtr,
         scriptName,
         wsBackIC,
@@ -41,6 +41,7 @@ def run(yes_to_all=False):
 
     end_time = time.time()
     print("\nRunning time: ", end_time - start_time, " seconds")
+    return scattResults, yFitResults
 
 
 def import_from_path(path, name):
