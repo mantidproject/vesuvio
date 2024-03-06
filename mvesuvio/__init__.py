c0806206b37a4d869b1e987d8c8a4ddd89d11e58
GuiMacielPereira <gui.maciel-pereira@stfc.ac.uk>
2024-03-06 11:57:00 +0000

    Corrected version file

--- mvesuvio/__init__.py
+++ mvesuvio/__init__.py
@@ -6,8 +6,7 @@ Vesuvio is an instrument that performs Neuton Compton Scattering, based at ISIS,
 nuclear kinetic energies and moment distributions.
 """
 
-version_info = (0, 0, 0)
-__version__ = ".".join(map(str, version_info))
+from mvesuvio._version import __version__
 __project_url__ = "https://github.com/mantidproject/vesuvio"
 
 from mvesuvio.scripts import main
