f49ef86299fe7dd2365d8b3398e11f85381880d1
GuiMacielPereira <gui.maciel-pereira@stfc.ac.uk>
2024-07-02 13:40:31 +0100

    Move tests to separate folder
    Moved all unit and system tests into its own folder.
    Had to change github workflows to include installing the
    mvesuvio package so that imports from unit and system
    tests work.
    Changed paths of imports to accomodate for new structure

---
+++ tests/calibration/unit/test_calibrate_vesuvio_analysis.py
@@ -0,0 +1,19 @@
+from tools.calibration_scripts.calibrate_vesuvio_analysis import EVSCalibrationAnalysis
+
+import unittest
+
+
+class TestVesuvioCalibrationAnalysis(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def test_placeholder(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
