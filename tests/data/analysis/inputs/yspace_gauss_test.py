be50cc3291d9f1aa3e800f0a444bac6e0d3e5e8f
Gui Maciel Pereira <80104863+GuiMacielPereira@users.noreply.github.com>
2024-12-13 13:41:44 +0000

    Use Minus() to subtract masses from total ncp (#150)
    * Use Minus to subtract masses from total ncp
    
    Using Minus algorithm makes sure that workspaces are compatible
    with each other, which is important for scientists when sometimes
    they have to subtract one workspace from another.
    
    It also improves readability of the code.
    
    * Update system tests for y-space fit
    
    Before overwriting the benchmarks I checked that my changes
    did not fail the previous system tests that were in place.
    Updated the inputs and the benchmarks of the system tests,
    because the previous inputs had workspaces that had unmatching
    units of Y and X.

--- tests/data/analysis/inputs/yspace_gauss_test.py
+++ tests/data/analysis/inputs/yspace_gauss_test.py
@@ -6,8 +6,15 @@ from tests.data.analysis.inputs.analysis_test import (
 ForwardAnalysisInputs.noOfMSIterations = 1
 ForwardAnalysisInputs.firstSpec = 164
 ForwardAnalysisInputs.lastSpec = 175
+ForwardAnalysisInputs.maskedSpecAllNo = [173, 174]
 ForwardAnalysisInputs.fit_in_y_space = True 
 BackwardAnalysisInputs.fit_in_y_space = False
 ForwardAnalysisInputs.run_this_scattering_type = True 
 BackwardAnalysisInputs.run_this_scattering_type = False
 YSpaceFitInputs.fitModel = "SINGLE_GAUSSIAN"
+
+if (__name__ == "__main__") or (__name__ == "mantidqt.widgets.codeeditor.execution"):
+    import mvesuvio
+    from pathlib import Path
+    mvesuvio.set_config(inputs_file=Path(__file__))
+    mvesuvio.run()
