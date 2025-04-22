0f3c41f550d305c4da62a6cca64acb18f76de7ae
Gui Pereira <guijose.pereira@gmail.com>
2025-04-22 13:49:58 +0100

    Add parse config var test

--- tests/unit/analysis/test_handle_config.py
+++ tests/unit/analysis/test_handle_config.py
@@ -9,6 +9,22 @@ class TestHandleConfig(unittest.TestCase):
     def setUpClass(cls):
         pass
 
+    def test_parse_config_env_var_with_env_variable(self):
+        with patch("mvesuvio.util.handle_config.os.getenv") as mock_getenv:
+            mock_getenv.return_value = "/some/random/path/to/vesuvio.user.properties"
+            # getattr because Python mangles name
+            config_path, config_file = getattr(handle_config, "__parse_config_env_var")()
+            self.assertEqual(str(config_path), "/some/random/path/to")
+            self.assertEqual(str(config_file), "vesuvio.user.properties")
+
+
+    def test_parse_config_env_var_default(self):
+        # getattr because Python mangles name
+        config_path, config_file = getattr(handle_config, "__parse_config_env_var")()
+        self.assertEqual(str(config_path), os.path.join(os.path.expanduser('~'), '.mvesuvio'))
+        self.assertEqual(str(config_file), "vesuvio.user.properties")
+
+
     def test_setup_default_inputs(self):
         tempdir = tempfile.gettempdir()
         mock_path = os.path.join(tempdir, 'mock_inputs.py')
